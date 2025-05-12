b7151f8245dda58fab3c28d9e5008f52b5dd3668
belac447 <caleb.atwood1@gmail.com>
2025-05-11 18:19:57 -0600

    Delete pages directory

--- pages/blog/chicken-beans-meal-prep-recipe.js
+++
@@ -1,6 +0,0 @@
-export default function Post() {
-  return <div>
-    <h1>Chicken Beans Meal Prep Recipe</h1>
-    <p>Paste your blog content here for "chicken-beans-meal-prep-recipe"</p>
-  </div>
-}
