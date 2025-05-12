ebfe471bf475c0021fadb87609ebf9ec4ba6ff00
belac447 <caleb.atwood1@gmail.com>
2025-05-11 18:01:46 -0600

    Delete pages directory

--- pages/blog/vegan-high-protein-batch-cooking.js
+++
@@ -1,6 +0,0 @@
-export default function Post() {
-  return <div>
-    <h1>Vegan High Protein Batch Cooking</h1>
-    <p>Paste your blog content here for "vegan-high-protein-batch-cooking"</p>
-  </div>
-}
