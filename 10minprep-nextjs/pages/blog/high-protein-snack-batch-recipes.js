bdea8a53ef1adca9a707b91204711908bf20bac7
belac447 <caleb.atwood1@gmail.com>
2025-05-11 18:02:39 -0600

    Add files via upload

---
+++ 10minprep-nextjs/pages/blog/high-protein-snack-batch-recipes.js
@@ -0,0 +1,6 @@
+export default function Post() {
+  return <div>
+    <h1>High Protein Snack Batch Recipes</h1>
+    <p>Paste your blog content here for "high-protein-snack-batch-recipes"</p>
+  </div>
+}
---
+++ 10minprep-nextjs/pages/blog/lentil-stew-high-protein-meal-prep.js
@@ -0,0 +1,6 @@
+export default function Post() {
+  return <div>
+    <h1>Lentil Stew High Protein Meal Prep</h1>
+    <p>Paste your blog content here for "lentil-stew-high-protein-meal-prep"</p>
+  </div>
+}
